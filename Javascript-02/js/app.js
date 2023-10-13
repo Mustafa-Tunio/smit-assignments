74220f4e9fd3904563efc75f2a8a24080bc5bd00
Mustafa-Tunio <ghulammustafatunio3@gmail.com>
2023-10-13 21:57:42 +0500

    update  assigment-02

--- Javascript-02/js/app.js
+++ Javascript-02/js/app.js
@@ -1,6 +1,4 @@
-
-
-// document.write("Assignment-01")
+document.write("Assignment-01")
 	
 // var a = 10;
 
@@ -31,10 +29,7 @@
 // document.write(a);
 
 
-
-
-// document.write("<br><br> Assignment-02 <br><br> ")
-
+document.write("<br><br> Assignment-02 <br><br> ")
 
 
 // var a = 2, b = 1
@@ -71,8 +66,7 @@
 // document.write(result);
 
 
-
-// document.write("<br><br> Assignment-03 <br><br> ")
+document.write("<br><br> Assignment-03 <br><br> ")
 
 
 // var  firstName = prompt("Enter Your Fristname")
@@ -83,14 +77,382 @@
 // document.write(fullName);
 
 
-// document.write("<br><br> Assignment-04 <br><br> ")
+
+document.write("<br><br> Assignment-04 <br><br> ")
 
 
 // var num = +prompt("Enter Any Number", "5")
-
 
 // for(var i = 1;  i <= 10; i++ ){
 // document.write(i + "x" + num + "=" + num * i + "<br>");
 // }	
 
-// obtain/ total *100
+
+document.write("<br><br> Assignment-05 <br><br> ");
+
+
+// document.write("<h2>Subject Total Marks Obatined Marks and Percantage</h2>")
+ 
+// var english = +prompt("Enter Your English Marks");
+// var math = +prompt("Enter Your Math Marks");
+// var urdu = +prompt("Enter Your Urdu Marks");
+
+// total = 100
+// subToatal = english+math+urdu
+
+// document.write("English" + " " + "100" +  "	" + english + "	" + english/total*total +"%" + "<br>" );
+// document.write("Math" + " " + "100" +  " " + math + " " + math/total*total +"%" + "<br>" );
+// document.write("Math" + " " + "100" +  " " + urdu + " " + urdu/total*total +"%" + "<br>" );
+
+
+// document.write(total*3 + " " + subToatal + " " +subToatal/300*100 + "%" );
+
+
+document.write("<br><br> Assignment-06 <br><br> ");
+
+// var city = prompt("Inter Your City").toLowerCase();	
+
+// if(city === "karachi"){
+// document.write("welcome to city of light");
+// }
+
+// if (city === "lahore"){
+// 	document.write("welcome to lahore")
+// }
+
+// document.write("<br><br> Assignment-07 <br><br> ");
+
+// var gender = prompt("Inter Your Gender").toLowerCase();	
+
+// var message = "Not Found!";
+
+// if( gender === "male"){
+//     message = "Good Morning Sir.";
+// }
+
+// if ( gender === "female"){
+// 	message ="Good Morning Ma'am.";
+// }
+
+// alert(message);
+
+
+document.write("<br><br> Assignment-08<br><br> ");
+
+// var signal = prompt("Inter your signal color");
+
+// var message = "";
+
+// if(signal === "Red"){
+// 	message = "Must Stop";
+// }
+// else if(signal === "Yellow"){
+// 	message = "Ready to Move";
+// }
+// else if( signal === "Green"){
+// 	message = "Move Now";
+// }
+// else{
+// 	message = "Dont Move Stop Until signal is Green"
+// }
+
+
+// console.log(message);	
+
+
+document.write("<br><br> Assignment-09<br><br> ");
+
+
+// var fuel = +prompt("Check remaining fuel in car");
+// var message = "";
+
+// if (fuel <= 1) {
+// 	message = "Please refill the fuel in your car"
+// }else if (fuel <= 2){
+// 	message = "remaining fuel is 2 Letter"
+// }else{
+// 	message = "Tanck is Full";
+// }
+
+// console.log(message);
+
+document.write("<br><br> Assignment-10<br><br> ");
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }else{
+// 	alert(" A=> condition is true")
+// }	
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }else{
+// 	alert(" B=> condition is not true just bcz post increament")
+// }	
+
+// var c = 12;
+
+// if (c++ === 13){
+// alert("condition 1 is true");
+
+// }else if(c === 13){
+// 	alert("condition 2 is true");
+
+// }else if (++c < 14){
+// 	alert("condition 3 is true");
+
+// }else if (c === 14) {
+// 	alert("condition 4 is true");
+
+// }else{
+// 	alert("C=> condition is not true just bcz post increament")
+// }
+
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+
+// if (true){
+// alert("True");
+// }
+
+// if (false){
+// alert("False if condition is false not show alert box message");
+// }
+
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+
+// document.write("<br><br> Assignment-11<br><br> ");
+
+// document.write("<h2>Mark Sheet</h2>");
+ 
+// var english = +prompt("Enter Your English Marks");
+// var math = +prompt("Enter Your Math Marks");
+// var urdu = +prompt("Enter Your Urdu Marks");
+
+// var total = 100*3
+// var subToatal = english+math+urdu;
+// var percentage = subToatal/total*100;
+
+// var grade = "";
+// var remarks = "";
+
+// total = 100*3
+// subToatal = english+math+urdu;
+
+
+// if (percentage >= 80) {
+//    grade = "A-One";
+//    remarks = "Excellent";
+// }else if( percentage >= 70 ){
+// 	grade = "A";
+// 	remarks ="Good";
+// }else if(percentage >= 60){
+// 	grade = "B";
+// 	remarks ="You Need To Improve";
+// }else if(percentage < 60){
+// 	grade = "Fail";
+// 	remarks ="Sorry";
+// }else(
+//   alert("Try Again Next Year")
+// )
+
+// document.write("<b>Total Marks:<b>" + "	" + total + "</br>");
+// document.write("<b>Obtain Marks:</b>" + "	" + subToatal + "</br>");
+// document.write("<b>Percentage:</b>" + "	" + subToatal/total*100+"%"  + "</br>");
+// document.write("<b>Grade:</b>" + "	" + grade  + "</br>");
+// document.write("<b>Remarks:</b>" + " 	" + remarks  + "</br>");
+
+// document.write("<br><br> Assignment-12<br><br> ");
+
+// var user = +prompt("Enter a Secret Number");
+// var	message = "";
+
+// if (user <= 10 || user <= +1 ) {
+// 	message = "Bingo! Correct Answer";
+// }else{
+// 	message = "Close enough to the correct answer"
+// }
+
+// console.log(message);
+
+// document.write("<br><br> Assignment-13<br><br> ");
+
+// var user = +prompt("Enter Your Number");
+
+// if(user % 3 === 0){
+// 	alert( user + "  " + "Is divisile By 3" );
+// }
+// else{
+// 	alert(user + "  " +  "Is not divisile By 3");
+// }
+
+document.write("<br><br> Assignment-14<br><br> ");
+
+
+// var user = +prompt("Enter Your Number");
+
+// if(user % 2 === 0){
+// 	alert( user + "  " + "This is Even Number" );
+// }
+// else{
+// 	alert(user + "  " +  "This is not Even No");
+// }	
+
+document.write("<br><br> Assignment-15<br><br> ");
+
+// var user = +prompt("Check out Temperature update")
+// message = "";
+
+// if (user >= 40) {
+// 	message = "It is too hot outside."
+// }else if ( user >= 30){
+// 	message = "The Weather today is Normal."
+// }else if (user >= 20) {
+// 	message = "Today’s Weather is cool."
+// }else if (user <= 10) {
+// 	message = "“OMG! Today’s weather is so Cool.”"
+// }else{
+// 	message = "Try Again"
+// }
+
+// alert(message);
+
+document.write("<br><br> Assignment-16<br><br> ");
+
+// var num = +prompt("Enter First value");	
+// var oprator = prompt("Enter Oprator");
+// var numTwo = +prompt("Enter Second value");
+
+// var result = "";
+
+// if (oprator === "/") {
+//    result =  num  / numTwo
+//    alert(result);
+// }else if(oprator === "*" ){
+// 	result = num * numTwo;
+// 	alert(result);
+// }else if (oprator === "%") {
+// 	result = num  % numTwo;
+// 	alert(result);
+// }else if(oprator === "+"){
+// 	result = num + numTwo;
+// 	alert(result);
+// }else if (oprator === "-") {
+// 	result = num - numTwo;
+// 	alert(result);
+// }else{
+// alert("Wrong Query");
+// }
+
+document.write("<br><br> Assignment-16<br><br> ");
+
+
+// var user = prompt("Enter a character (number or letter):")
+// var output = user.charCodeAt(0);
+
+// console.log(output);
+
+// if (output >= 48 && output <= 57){
+// 	console.log(user + "	" + " is a number.");
+// }
+// else if(output >= 65 && output <= 90){
+// 	console.log(user + "	" + "Is an Uppercase Leeter");
+// }
+// else if (output >= 97 && output <= 122){
+// 	console.log(user + "	" + "Is an Lowercase letter");
+// }
+// else{
+// 	console.log("You Put it wrong number");	
+// }
+
+document.write("<br><br> Assignment-17<br><br> ");
+
+// var num = +prompt("Enter a First  Value ");
+// var num2 = +prompt("Enter a Second value");
+
+
+// if(num > num2){
+// 	console.log(num + " " + "greater than" + " " + num2)
+// }else if(num > num2 ){
+// 	console.log(num + " " + "greater than" + " " + num2)
+// }else if (num === num2) {
+// 	console.log(num + " " + "Equal to" + " " + num2)
+// }else if (num < num2) {
+// 	console.log(num + " " + "Less than" + " " + num2)
+// }
+
+document.write("<br><br> Assignment-18<br><br> ");
+
+// var num = +prompt("Enter a First  Value ");
+
+
+// if (num < 0) {
+// 	console.log(num + "	" + "is Negative Number")
+// }else if(num > 0 ){
+// 	console.log(num + "	" + "is Positave Number")
+// }else if(num === 0){
+// 	console.log(num + "	" + "is not value")
+// }
+
+// var user =prompt("Enter any letter");
+// user = user.toLowerCase();
+// var message = "";
+
+// if (user.length === 1 &&  user === "a" || user === "e" || user === "i" || user === "o" || user === "u"){
+    
+//     console.log(user + "	" + "its a vowel Letter");
+// }else{
+// 	console.log(user + "	" + "its not vowel letter");
+// }
+
+document.write("<br><br> Assignment-19<br><br> ");
+
+// var correctpswrd = "Mypassword"
+
+// var paswrd = prompt("Enter Your password");
+
+// if(correctpswrd === paswrd){
+// 	alert("Correct! The password you entered matches the original password.")
+// }else{
+// 	alert("Your Enter Wrong password");
+// }
+
+
+document.write("<br><br> Assignment-20<br><br> ");
+
+
+// var greeting;
+// var hour = 13;
+
+// if (hour < 18) { 
+// 	greeting = "Good day";
+// }else{
+// 	greeting = "Good evening";
+// }
+
+// console.log(greeting)
+
+document.write("<br><br> Assignment-21<br><br> ");
+
+// var hour = prompt("Check Out Time level");
+ 
+// if (hour >= "0000" && hour < 1200) {
+// 	console.log("Good Morning!");
+// }else if(hour >= 1200 && hour < 1700){
+// 	console.log("Good Afternoon!");
+// }else if (hour >= 1700 && hour < 2100) {
+// 	console.log("Good Evening!");
+// }else if(hour >= 2100 && hour <= 2359){
+// 	console.log("Good Night!");
+// }
